5e2267c9a3bfd1d13f499fda005bca367449956a
Annija Aumale <aumale.annija@gmail.com>
2025-03-12 15:23:12 +0200

    first commit

---
+++ src/main/java/lv/venta/model/Product.java
@@ -0,0 +1,17 @@
+package lv.venta.model;
+
+public class Product {
+	//Variables
+	private long id;
+
+	private String title;
+	private float price ;
+	private  String description;
+	private int quantity;
+	// getters
+	
+	//setters
+	
+	//
+
+}
