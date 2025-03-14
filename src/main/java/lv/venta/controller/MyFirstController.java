5ef19f67b28864a0027327202f4c74c3893f1cee
Annija Aumale <aumale.annija@gmail.com>
2025-03-14 14:04:38 +0200

    amodified

--- src/main/java/lv/venta/controller/MyFirstController.java
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -1,27 +1,57 @@
 package lv.venta.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lv.venta.model.Product;
 
 @Controller
 public class MyFirstController {
 	
-	@GetMapping("/simple") 
+	
+	@GetMapping("/simple") //localhost:8080/simple
 	public String getControllerSimple() {
-		System.out.println("Simple Controller is called");
-		return "simple-page";
+		System.out.println("Simple controller is called");
+		return "simple-page";//it will show a simple-page.html page
+		
 	}
+	
 	
 	@GetMapping("/data") //localhost:8080/data
 	public String getControllerData(Model model) {
-		Random rand =new Random();
+		Random rand = new Random();
 		System.out.println("Data controller is called");
-		String data = "Martin :"  + rand.nextInt(0,11);
+		String data  = "Martin: " + rand.nextInt(0, 11);
 		model.addAttribute("box", data);
-		return "data-page";
+		return "data-page";//it will show a data-page.html with data "Karina: 6"
 	}
+	
+	@GetMapping("/testproduct")//localhost:8080/testproduct
+	public String getControllerSendProduct(Model model) {
+		Product testProduct = new Product("Banana", 1.99f, "Eco, yellow", 5);
+		model.addAttribute("box", testProduct);
+		return "show-one-product-page";//it will show a show-one-product-page.html with test product (Banana, 1.99 eur ...)
+		
+	}
+	
+	@GetMapping("/testallproducts")//localhost:8080/testallproducts
+	public String getControllerSendMultipleProducts(Model model)
+	{
+		ArrayList<Product> allProducts = new ArrayList<Product>(
+				Arrays.asList(
+						new Product("Banana", 1.99f, "Eco, yellow", 5),
+						new Product("Grapes", 4.99f, "Purple", 10),
+						new Product("Watermelon", 5.99f, "Sweet", 2))
+				);
+		
+		model.addAttribute("box", allProducts);
+		return "show-all-product-page";//it will show a show-all-product-page.html with arraylist of products
+
+	}
+
 }
--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,17 +1,77 @@
 package lv.venta.model;
 
-public class Product {
-	//Variables
-	private long id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+
+@Table(name = "ProductTable") //mysql - product_table, H2 - PRODUCT_TABLE
+@Entity
+public class Product {
+	//1. variables
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)//<-this means that setter for id will be not visible/usable
+	private long id; //TODO auto increment from DB
+	
+	
+
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-z ]{2,20}")
+	//@Size(min = 3, max = 21) <- I can use it if there is no min or max inside the regex
+	@Column(name = "Title")
 	private String title;
-	private float price ;
-	private  String description;
+	
+	
+	
+	@Min(0)
+	@Max(1000)
+	@Column(name = "Price")
+	private float price;
+	
+	@NotNull
+	@Pattern(regexp = "[A-Za-z0-9 ,.;:]+")
+	@Size(min = 5, max = 100)
+	@Column(name = "Description")
+	private String description;
+	
+	
+	@Min(0)
+	@Max(100)
+	@Column(name = "Quantity")
 	private int quantity;
-	// getters
+	//2. getters - lombok
+	//3. setters - lombok
+	//4. no argument constructor - lombok
+	//5. argument constructors
+	public Product(String inputTitle, float inputPrice, String inputDescription, 
+			int inputQuantity)
+	{
+		setTitle(inputTitle);
+		setPrice(inputPrice);
+		setDescription(inputDescription);
+		setQuantity(inputQuantity);
+	}
+	//6. toString function - lombok
+	//7. other functions (if it is neccessary)
 	
-	//setters
-	
-	//
 
 }
